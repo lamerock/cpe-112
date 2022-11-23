18d894d701833635e7ac6349b24573f31abfc36a
Jamz <2452534+lamerock@users.noreply.github.com>
2022-11-23 16:51:54 +0800

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,42 @@
+person_name = "James"
+person_age = "65"
+
+print("There was a man named " + person_name + ".")
+print("He was " + person_age + " years old.")
+print("He really like his name, " + person_name + ".")
+print("But he does not want his age, " + person_age + ".")
+
+print(5 + 6)
+print(5 + 6.0)
+print(5 - 6)
+print(5 * 6)
+print(5 / 6)
+print(27 % 4) # modulo or getting the remainder
+
+print(5 > 6)    # False
+print(5 < 6)    # True
+print(5 == 6)   # False
+print(5 != 6)   # True
+
+
+if (6 < 5):
+  print("Yes. 6 is greater than 5.")
+  print("Thank you  !")
+
+v = 3j        # complex number
+w = "Python"  # string characters
+x = 3         # int or integer
+y = 4.2       # float
+z = False     # bool type / Boolean - True or False
+
+print(type(v))
+print(type(w))
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = "3"
+y = 5
+print(x + str(y))
+print(int(x) + y)
+print(float(x) + y)
